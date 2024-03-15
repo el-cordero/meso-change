46f8fb2c9f6e247aef4bdddbc22f7c124be3b055
el-cordero <cordero.elvin@gmail.com>
2024-03-15 06:47:55 +0530

    commit

---
+++ _scripts/01_functions/random_forest_raster.R
@@ -0,0 +1,38 @@
+random_forest_raster <- function(r, df, na_rows, model_date){
+    # set the seed
+    set.seed(99)
+    df_clean <- na.omit(df[-na_rows,])
+
+    # Partition the data for training and testing
+    # 80% of the data will be used for the training
+    data_split <- initial_split(df_clean, prop = 0.80, strata=class)  # Adjust the proportion as needed
+    train_data <- training(data_split)
+    test_data <- testing(data_split)
+
+    rf_model_spec <- rand_forest() %>%
+    set_engine("ranger") %>%
+    set_mode("classification")  # Use "regression" for a regression problem
+
+    rf_recipe <- recipe(class ~ ., data = train_data) %>%
+    step_normalize(all_predictors())  # Example step to normalize predictors
+
+    rf_workflow <- workflow() %>%
+    add_model(rf_model_spec) %>%
+    add_recipe(rf_recipe) %>%
+    fit(data = train_data)
+
+    rf_results <- predict(rf_workflow, new_data = test_data) %>%
+    bind_cols(test_data) %>%  
+    metrics(truth = class, estimate = .pred_class)
+    rf_results$model <- model_date
+
+    predictions <- predict(rf_workflow, new_data = df[-na_rows,-1])  # Combine predictions with true outcomes for evaluation
+    df$predictions <- NULL
+    df[-na_rows,'predictions'] <- predictions$.pred_class
+
+    rf_raster <- init(r[[1]],NA)
+    values(rf_raster) <- df_original$predictions
+    names(rf_raster) <- model_date
+
+    return(list(raster = rf_raster, results = rf_results))
+}
--- _scripts/03_data_analysis/RandomForest_planet_22_dates.R
+++ _scripts/03_data_analysis/RandomForest_planet_22_dates.R
@@ -4,30 +4,27 @@ source('_scripts/01_functions/raster_prep.R')
 planet <- rast('_data/GIS/Raster/Clean/planet_22dates.tif')
 planet <- trim(planet)
 
-planet_median <- rast('_data/GIS/Raster/Clean/planet_22dates_median.tif')
-planet_median <- trim(planet_median)
-
-s <- rast(ext=ext(planet_median),crs=crs(planet_median),res=50)
+s <- rast(ext=ext(planet),crs=crs(planet),res=10)
 planet <- resample(planet,s)
-planet_median <- resample(planet_median,s)
 
-
-# for (i in 1:nlyr(planet_median)) planet_median <- mask(planet_median,planet_median[[i]])
+# for (i in 1:nlyr(planet)) planet <- mask(planet,planet[[i]])
 planet <- scale(planet)
-planet_median <- scale(planet_median)
+
+dates <- sub("^(\\d{8})_.*$", "\\1", names(planet))
+dates <- unique(dates)
 
 # inputed user polygons with observations
 userData <- vect('_data/GIS/Vector/userDelineated.shp')
-userData <- terra::project(userData, crs(planet_median))
+userData <- terra::project(userData, crs(planet))
 
 # change userData classes to factor to retain class values
 userData$class <- as.factor(userData$class)
 
 # add as a raster layer to raster
-planet_median[['class']]  <- rasterize(userData, planet_median, field = "class")
+planet[['class']]  <- rasterize(userData, planet, field = "class")
 
 # create dataframe from raster stack
-df_original <- as.data.frame(planet_median, cell=FALSE,na.rm=FALSE)
+df_original <- as.data.frame(planet, cell=FALSE,na.rm=FALSE)
 
 # change classes to factor to retain class values
 df_original$class <- as.factor(df_original$class)
@@ -46,7 +43,7 @@ set.seed(99)
 
 # Partition the data for training and testing
 # 80% of the data will be used for the training
-data_split <- initial_split(df_clean, prop = 0.80)  # Adjust the proportion as needed
+data_split <- initial_split(df_clean, prop = 0.80, strata=class)  # Adjust the proportion as needed
 train_data <- training(data_split)
 test_data <- testing(data_split)
 
@@ -62,119 +59,65 @@ rf_workflow <- workflow() %>%
   add_recipe(rf_recipe) %>%
   fit(data = train_data)
 
+rf_results <- predict(rf_workflow, new_data = test_data) %>%
+  bind_cols(test_data) %>%  
+  metrics(truth = class, estimate = .pred_class)
+rf_results$model <- 'baseline'
+
 predictions <- predict(rf_workflow, new_data = df_original[-na_rows,-1])  # Combine predictions with true outcomes for evaluation
 df_original$predictions <- NULL
 df_original[-na_rows,'predictions'] <- predictions$.pred_class
 
-rf_raster <- init(planet_median[[1]],NA)
+rf_raster <- init(planet[[1]],NA)
 values(rf_raster) <- df_original$predictions
-names(rf_raster) <- 'randomforest'
+names(rf_raster) <- 'baseline'
+
+# set the seed
+set.seed(99)
 
 for (i in 1:22){
-  subset_raster <- c((1:4)+4*(i-1))
-  subset_df <- as.data.frame(planet[[subset_raster]], cell=FALSE,na.rm=FALSE)
-  subset_na <- which(apply(subset_df, 1, function(x) any(is.na(x))))
-  names(subset_df) <- c("blue", "green", "red", "nir")
-  subset_pred <- predict(rf_workflow, new_data = subset_df[-subset_na,])  # Combine predictions with true outcomes for evaluation
+  subset_cols <- c(1,1+(1:4)+4*(i-1))
+  subset_df <- df_original[,subset_cols]
+  # 'na_rows' now contains the indices of rows with any NA values
+  subset_df_clean <- na.omit(subset_df[-na_rows,])
+
+  # Partition the data for training and testing
+  # 80% of the data will be used for the training
+  subset_data_split <- initial_split(subset_df_clean, 
+    prop = 0.80, ,strata=class)
+  subset_train_data <- training(subset_data_split)
+  subset_test_data <- testing(subset_data_split)
+
+  subset_recipe <- recipe(class ~ ., data = subset_train_data) %>%
+    step_normalize(all_predictors())  
+
+  subset_workflow <- workflow() %>%
+    add_model(rf_model_spec) %>%
+    add_recipe(subset_recipe) %>%
+    fit(data = subset_train_data)
+
+  subset_pred <- predict(subset_workflow, new_data = subset_df[-na_rows,-1])  # Combine predictions with true outcomes for evaluation
   subset_df$predictions <- NULL
-  subset_df[-subset_na,'predictions'] <- subset_pred$.pred_class
-  rf_raster[[paste0('date_',i+1)]] <- init(rf_raster[[1]],NA)
-  values(rf_raster[[paste0('date_',i+1)]]) <- subset_df$predictions
-}
+  subset_df[-na_rows,'predictions'] <- subset_pred$.pred_class
 
-plot(rf_raster[[8]])
+  rf_raster[[dates[i]]] <- init(rf_raster[[1]],NA)
+  values(rf_raster[[dates[i]]]) <- subset_df$predictions
 
-# For classification, you might calculate accuracy, ROC AUC, etc.
-rf_results <- predictions %>%
-  metrics(truth = class, estimate = .pred_class)  # Adjust based on your outcome variable and predictions
+  subset_rf_results <- 
+    predict(subset_workflow, new_data = subset_test_data) %>%
+    bind_cols(subset_test_data) %>%  
+    metrics(truth = class, estimate = .pred_class)
 
-# For detailed class-specific metrics
-rf_class_metrics <- predictions %>%
-  roc_auc(class, .pred_class)  # Adjust based on your needs
-
-
-
-# set training control parameters
-fitControl <- trainControl(method="repeatedcv", number=5, repeats=5)
-
-# set clusters
-cl <- makePSOCKcluster(5)
-
-# parallel programming
-registerDoParallel(cl)
-
-# train the random forest model for the complete dataframe
-rfModel <- train(class~.,data=training, method="rf",
-                  trControl=fitControl,
-                  prox=TRUE,
-                  fitBest = FALSE,
-                  returnData = TRUE)
-
-# stop the clusters
-stopCluster(cl)
-
-# save the model for future use
-pathRF <- "~/Documents/Projects/USACE/ML Mesohabitats/Data/ML/Models/Random Forest/"
-saveRDS(rfModel, file = paste0(pathRF,"rf_model_all.rds"))
-
-
-# remove class raster
-r <- r[[-(nlyr(r))]]
-
-# run the model on the raster
-rasterRF <- predict(r,rf_workflow,na.rm=TRUE)
-
-# rename the raster layer
-names(rasterRF[[1]]) <- 'date_all'
-
-# train random forest model for each date and apply model to that date
-for (i in 1:length(rasterFiles)){
-  
-  # subset class column AND the 7 columns pertaining to bands for a date
-  subsetDate.df <- c(1,(2:8)+7*(i-1))
-  
-  # parallel processing
-  registerDoParallel(cl)
-  
-  # train for date subset
-  rfModel.dt <- train(class~.,data=training[,subsetDate.df], 
-                           method="rf",
-                            trControl=fitControl,
-                            prox=TRUE,
-                            fitBest = FALSE,
-                            returnData = TRUE)
-  
-  # save the random forest model
-  saveRDS(rfModel.dt, file = paste0(pathRF,"rf_model_",i,".rds"))
-  
-  # # run the model on testing data
-  # pred_rf <- predict(rf_model_single$finalModel, 
-  #                    newdata = testing[,subsetDate.df])
-  
-  # subset for date
-  subsetDate.r <- (1:7)+7*(i-1)
-  
-  # run the model on the raster
-  # add as a raster layer
-  rfDate <- paste0("date_",i)
-  rasterRF[[rfDate]] <- predict(r[[subsetDate.r]],rfModel.dt,
-                                na.rm=TRUE)
-  
-  # stop the cluster
-  stopCluster(cl)
-}
-
-# landsat files list
-rasterNames <- list.files(path=paste0(raster,'Landsat/Raw/'),
-                          pattern=".tif$", full.names = FALSE)
-names(rasterRF) <- sub(".tif.*", "", rasterNames)
-
-for (i in 1:length(names(rasterRF))){
-  plot(rasterRF[[i]])
+  subset_rf_results$model <- dates[i]
+  rf_results <- rbind(rf_results,subset_rf_results)
 }
 
 
+# check the performance
+acc <- rf_results %>% filter(.metric == 'accuracy', model != 'baseline')
+kapa <- rf_results %>% filter(.metric == 'kap', model != 'baseline')
+plot(acc$.estimate,type='l')
+plot(kapa$.estimate,type='l')
 
-
-writeRaster(rasterRF,paste0(raster,'Analysis/RandomForest/combined/rfStack.tif'),
+writeRaster(rf_raster,paste0(raster,'Analysis/RandomForest/combined/rfStack.tif'),
             overwrite=TRUE)
